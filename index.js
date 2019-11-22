84876dd204fa15215d99305153fc5f2dc5c7b439
yousif diyhaa <yousif7733@gmail.com>
2019-11-22 00:22:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+document.addEventListener( "DOMContentLoaded", function () {
+  updateDOM()
+} );
+
+
+function updateDOM() {
+  document.getElementById( "text" )
+    .innerHTML = "This is really cool!";
+}
